ad42ff175adc98d4b8cdfb09aaefac1c9a7062c6
Saleh0831 <105693052+Saleh0831@users.noreply.github.com>
2023-06-19 15:04:21 +0330

    slideProductr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UiProduct';
+url:string='../assets/Carousel_iPhone14_Blue_Placement01-PreOrder.webp';
+
+  changeImage(e:any){
+    console.log(e.target.src);
+    this.url=e.target.src;
+  }
 }
